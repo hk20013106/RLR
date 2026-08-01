76e82c5f85309d0ddeb5617317870fb6d9b6d128
Kai He <hk200131060071@163.com>
2026-08-01 22:33:21 +0800

    docs: install topology annotations for new flow

--- src/research_loop/__init__.py
+++ src/research_loop/__init__.py
@@ -18,6 +18,11 @@ from research_loop.method_contracts import install as _install_method_contracts
 
 _install_method_contracts(hypothesis_contracts)
 
+from research_loop import topology as topology
+from research_loop.topology_extensions import install as _install_topology_extensions
+
+_install_topology_extensions(topology)
+
 from research_loop.commands import lifecycle as _lifecycle
 from research_loop import context as _context
 from research_loop.conditional_routing import install as _install_conditional_routing
@@ -26,4 +31,5 @@ _install_conditional_routing(_lifecycle, _context)
 
 del _install_method_evidence, _install_method_evidence_compat
 del _install_review_navigation, _registered_sources
-del _install_method_contracts, _install_conditional_routing, _lifecycle, _context
+del _install_method_contracts, _install_topology_extensions
+del _install_conditional_routing, _lifecycle, _context
