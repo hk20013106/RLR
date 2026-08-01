4b0104b23f1265008358c3f43508ed0df089dd38
Kai He <hk200131060071@163.com>
2026-08-01 22:28:45 +0800

    feat: install review navigation semantics

--- src/research_loop/__init__.py
+++ src/research_loop/__init__.py
@@ -5,9 +5,13 @@
 from research_loop import deep_research as deep_research
 from research_loop.method_evidence import install as _install_method_evidence
 from research_loop.method_evidence_compat import install as _install_method_evidence_compat
+from research_loop.method_review_navigation import install as _install_review_navigation
+from research_loop.user_sources import registered_sources as _registered_sources
 
 _install_method_evidence(deep_research)
 _install_method_evidence_compat(deep_research)
+deep_research.registered_sources = _registered_sources
+_install_review_navigation(deep_research)
 
 from research_loop import hypothesis_contracts as hypothesis_contracts
 from research_loop.method_contracts import install as _install_method_contracts
@@ -21,4 +25,5 @@ from research_loop.conditional_routing import install as _install_conditional_ro
 _install_conditional_routing(_lifecycle, _context)
 
 del _install_method_evidence, _install_method_evidence_compat
+del _install_review_navigation, _registered_sources
 del _install_method_contracts, _install_conditional_routing, _lifecycle, _context
