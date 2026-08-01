fa7ad7889bbc35e340f48741d4e78218d6421131
Kai He <hk200131060071@163.com>
2026-08-01 22:24:14 +0800

    fix: install user-source evidence normalization

--- src/research_loop/__init__.py
+++ src/research_loop/__init__.py
@@ -4,8 +4,10 @@
 # their functions and schemas.
 from research_loop import deep_research as deep_research
 from research_loop.method_evidence import install as _install_method_evidence
+from research_loop.method_evidence_compat import install as _install_method_evidence_compat
 
 _install_method_evidence(deep_research)
+_install_method_evidence_compat(deep_research)
 
 from research_loop import hypothesis_contracts as hypothesis_contracts
 from research_loop.method_contracts import install as _install_method_contracts
@@ -18,5 +20,5 @@ from research_loop.conditional_routing import install as _install_conditional_ro
 
 _install_conditional_routing(_lifecycle, _context)
 
-del _install_method_evidence, _install_method_contracts
-del _install_conditional_routing, _lifecycle, _context
+del _install_method_evidence, _install_method_evidence_compat
+del _install_method_contracts, _install_conditional_routing, _lifecycle, _context
