c46b66a26f63711fbdc1108f8309437bcacf278c
Kai He <hk200131060071@163.com>
2026-08-02 11:14:29 +0800

    fix: install L3 reactivation lineage checks

--- src/research_loop/__init__.py
+++ src/research_loop/__init__.py
@@ -36,9 +36,13 @@ from research_loop.ledger_receipt_idempotency import (
 from research_loop.hypothesis_reactivation import (
     install as _install_hypothesis_reactivation,
 )
+from research_loop.hypothesis_reactivation_constraints import (
+    install as _install_reactivation_constraints,
+)
 
 _install_receipt_idempotency(hypothesis_ledger)
 _install_hypothesis_reactivation(hypothesis_ledger)
+_install_reactivation_constraints(hypothesis_ledger)
 
 from research_loop import topology as topology
 from research_loop.topology_extensions import install as _install_topology_extensions
@@ -69,6 +73,7 @@ del _review_navigation, _install_navigation_compat
 del _install_review_status_compat, _registered_sources
 del _install_method_contracts, _install_reactivation_contracts
 del _install_receipt_idempotency, _install_hypothesis_reactivation
+del _install_reactivation_constraints
 del _install_topology_extensions, _install_conditional_routing
 del _install_hypothesis_recall_context, _install_hypothesis_pool_cli
 del _lifecycle, _ledger_commands, _context, _cli
